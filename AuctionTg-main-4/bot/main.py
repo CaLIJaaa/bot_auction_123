c279a9922dbbbe9d0fab86cc38a07afff72c0da7
CaLIJaaa <000lobovsasha000@mail.ru>
2024-06-15 19:14:45 +0300

    qqq

--- AuctionTg-main-4/bot/main.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from app import bot
-import logging
-import sys
-import app.DB
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(bot.main())
